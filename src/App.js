e351c2ef2eb6cd16e4e1889986822cfacbc0559d
Miroslavetsh <dfgshte@gmail.com>
2023-07-30 22:23:29 +0300

    Use context instead of state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import React, { useState } from 'react'
-import { v4 } from 'uuid'
-
-import colorData from './data/colors.json'
-import ColorList from './components/ColorList'
-import AddColorForm from './components/AddColorForm'
+import React from 'react'
+import ColorList from './ColorList.js'
+import AddColorForm from './AddColorForm'
 
 export default function App() {
-  const [colors, setColors] = useState(colorData)
   return (
     <>
-      <AddColorForm
-        onNewColor={(title, color) => {
-          const newColors = [
-            ...colors,
-            {
-              id: v4(),
-              rating: 0,
-              title,
-              color,
-            },
-          ]
-          setColors(newColors)
-        }}
-      />
-      <ColorList
-        colors={colors}
-        onRemoveColor={(id) => {
-          const newColors = colors.filter((color) => color.id !== id)
-          setColors(newColors)
-        }}
-      />
+      <AddColorForm />
+      <ColorList />
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext } from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import colors from './data/colors.json'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+import './index.css'
+
+export const ColorContext = createContext()
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <ColorContext.Provider value={{ colors }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ColorContext.Provider>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
